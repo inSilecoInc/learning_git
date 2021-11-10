75f1712260b8e139f665da69fab6f11f5ff9df84
Kevin Cazelles <kcazelle@uoguelph.ca>
2021-11-10 16:46:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Template for R workshops
+![deploy workshop](https://github.com/inSilecoInc/workshop_R_template/workflows/deploy%20workshop/badge.svg)
+
+
+## Installation 
+
+`iseWorkshops` must be installed. 
+
+```{R}
+install.packages("remotes")
+remotes::install_github("iseWorkshops")
+```
---
+++ _setup.R
@@ -0,0 +1,14 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  dev = "png",
+  dpi = 108,
+  fig.width = 6,
+  fig.height = 4.5,
+  fig.align = 'center',
+  width = 120
+)
+options(htmltools.dir.version = FALSE, htmltools.preserve.raw = FALSE)
+library(iseWorkshops)
